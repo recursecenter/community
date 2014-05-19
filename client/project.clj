0deaf180aab6103f840624fcafe613d0bb167879
David Albert <davidbalbert@gmail.com>
2014-05-19 11:11:12 -0400

    Downgrade to CLJS 2173 to fix LightTable

--- client/project.clj
+++ client/project.clj
@@ -1,11 +1,11 @@
 (defproject community "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2202"]
+                 [org.clojure/clojurescript "0.0-2173"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [om "0.6.2"]
                  [cljs-ajax "0.2.4"]]
 
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
 
   :cljsbuild {
     :builds [{:id "community-dev"
