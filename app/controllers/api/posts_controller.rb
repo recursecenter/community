08438aced2af51f145371b2bcbce20a0a9668bcf
David Albert <davidbalbert@gmail.com>
2014-05-21 19:50:30 -0400

    Search for thread inside posts#create

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,11 +1,12 @@
 class Api::PostsController < Api::ApiController
   def create
-    @post = current_user.posts.create(post_params)
+    thread = DiscussionThread.find(params[:thread_id])
+    @post = current_user.posts.create(post_params.merge(thread: thread))
   end
 
 private
 
   def post_params
-    params.require(:post).permit(:body).merge(thread_id: params[:thread_id])
+    params.require(:post).permit(:body)
   end
 end
