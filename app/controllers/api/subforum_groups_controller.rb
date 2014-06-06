8d91e706252958925c77d79845d67bc1b6147b83
Zachary Allaun <zach.allaun@gmail.com>
2014-06-06 07:38:45 -0400

    `@subforum_groups` query now include `subforums_with_visited_status`

--- app/controllers/api/subforum_groups_controller.rb
+++ app/controllers/api/subforum_groups_controller.rb
@@ -2,6 +2,6 @@ class Api::SubforumGroupsController < Api::ApiController
   load_and_authorize_resource :subforum_group
 
   def index
-    @subforum_groups = @subforum_groups.includes(:subforums)
+    @subforum_groups = @subforum_groups.includes(:subforums_with_visited_status)
   end
 end
