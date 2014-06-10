3985ffdb1fcfbb177f7264ab7b0bc701099876ba
David Albert <davidbalbert@gmail.com>
2014-06-10 11:45:55 -0400

    Updated Heroku recommended Puma config.

--- config/puma.rb
+++ config/puma.rb
@@ -10,7 +10,10 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # worker specific setup
   ActiveSupport.on_load(:active_record) do
-    ActiveRecord::Base.establish_connection
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['MAX_THREADS'] || 16
+    ActiveRecord::Base.establish_connection(config)
   end
 
   uri = URI.parse(ENV["REDIS_URL"])
