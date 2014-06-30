d8220ad0717d232c9d91d9bc1df65ffd32081ce7
Zachary Allaun <zach.allaun@gmail.com>
2014-06-30 11:09:12 -0400

    Add docstrings to transform macros, remove commented out code

--- client/src/community/util/transform.clj
+++ client/src/community/util/transform.clj
@@ -1,7 +1,10 @@
 (ns community.util.transform
   (:require [community.util :refer [p]]))
 
-(defmacro deftransformer [name]
+(defmacro deftransformer
+  "Defines a top-level transformer called `name`. If you want a
+  local/anonymous transformer, use `community.util.transform/transformer`."
+  [name]
   `(def ~name (community.util.transform/transformer)))
 
 (defn wrap-vec [value]
@@ -22,7 +25,27 @@
                    (for [k (:many key-spec-map)]
                      {:key k :single-key single-key :many? true}))))))
 
-(defmacro deftransform [name key-spec param-binding & body]
+(defmacro deftransform
+  "Define a new transform for a given transformer. `key-spec` can have any
+  of the following formats:
+
+    :keyword -> will transform values keyed by :keyword
+    [:k1 :k2 ...] -> will transform values keyed by :k1, :k2, ...
+    {:single :k, :many :ks} -> will transform values keyed by :k, or will
+                               map a :k transform over a vector keyed by :ks
+    {:single [:k1 :k2 ...], :many [:ks :ys]} -> as above with multiple keys
+
+  `param-binding` can be a symbol or a destructuring form.
+
+  e.g.
+
+    (deftransform my-transformer {:single [:x, :y] :many [:xs :ys]}
+      {:keys [a b c]}
+      (+ a b c))
+
+    (my-transformer {:x {:a 1 :b 2 :c 3} :ys [{:a 1 :b 1 :c 1}]})
+      => {:x 6 :ys [3]}"
+  [name key-spec param-binding & body]
   `(let [handler# (fn [~param-binding] ~@body)]
      ~@(for [{:keys [many? key single-key]} (normalize-key-specs key-spec)]
         (if many?
@@ -30,11 +53,3 @@
             ~name ~key (fn [param#] (mapv #(~name ~single-key %) param#)))
           `(community.util.transform/-add-transform
             ~name ~key (fn [~param-binding] ~@body))))))
-
-;; (defmacro deftransform [name key param-binding & body]
-;;   `(community.util.transform/-add-transform ~name ~key
-;;      (fn [~param-binding] ~@body)))
-
-;; PROBLEM
-;; I'm getting a weird "invalid arity 1" error
-;; the commented out version works
