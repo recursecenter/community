2819b6bcc1ab60205c9404bdee92eb1d3df8d7c7
David Albert <davidbalbert@gmail.com>
2014-07-02 17:57:17 -0400

    Fix syntax

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,11 +86,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "community.hackerschool.com", protocol: "https" }
 
   config.action_mailer.smtp_settings = {
-    :address => ENV["SMTP_SERVER"],
-    :port => ENV["SMTP_PORT"],
-    :user_name => ENV["SMTP_USERNAME"],
-    :password => ENV["SMTP_PASSWORD"],
-    :authentication => :plain,
-    :enable_starttls_auto: true
+    address: ENV["SMTP_SERVER"],
+    port: ENV["SMTP_PORT"],
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
