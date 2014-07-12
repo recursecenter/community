2b7026f373f82dcb6c62904a10faa3c6e19b3fd5
Zachary Allaun <zach.allaun@gmail.com>
2014-07-12 00:04:46 -0400

    Remove print

--- app/controllers/api/settings_controller.rb
+++ app/controllers/api/settings_controller.rb
@@ -2,7 +2,6 @@ class Api::SettingsController < Api::ApiController
   skip_authorization_check only: :update
 
   def update
-    puts settings_params
     current_user.update!(settings_params)
     render json: {}
   end
