036a62086fa087cda9c4ead1d8fe4758df6ca070
Zachary Allaun <zach.allaun@gmail.com>
2014-07-11 21:24:00 -0400

    Fix bugs in notification coordinator dedup logic

--- app/services/notification_coordinator.rb
+++ app/services/notification_coordinator.rb
@@ -16,7 +16,7 @@ class NotificationCoordinator
       notifiers.each do |n|
         if n.possible_recipient?(u) && n.should_email?(u)
           email_recipients[n] << u
-          next
+          break
         end
       end
     end
@@ -30,6 +30,6 @@ private
   def users
     @users ||= notifiers.map do |n|
       n.possible_recipients
-    end.flatten.to_set
+    end.inject(Set.new) { |s1, s2| s1 | s2 }
   end
 end
