68ce66005369b0c2f77b4fc79fd613e2c9dfedbe
Zachary Allaun <zach.allaun@gmail.com>
2014-07-13 11:22:04 -0400

    Correctly delay batched mails (fixed #66)

--- app/notifiers/subforum_subscription_notifier.rb
+++ app/notifiers/subforum_subscription_notifier.rb
@@ -9,12 +9,19 @@ class SubforumSubscriptionNotifier < Notifier
 
   def notify(email_recipients)
     unless email_recipients.empty?
-      mail = NotificationMailer.new_thread_in_subscribed_subforum_email(email_recipients, thread)
-      BatchMailSender.new(mail).delay.deliver
+      Delayed::Job.enqueue SubforumSubscriptionJob.new(email_recipients, thread)
     end
   end
 
   def possible_recipients
     @possible_recipients ||= thread.subforum.subscribers.select { |u| Ability.new(u).can? :read, thread }.to_set
+  end
+
+private
+  SubforumSubscriptionJob = Struct.new(:email_recipients, :thread) do
+    def perform
+      mail = NotificationMailer.new_thread_in_subscribed_subforum_email(email_recipients, thread)
+      BatchMailSender.new(mail).deliver
+    end
   end
 end
--- app/notifiers/thread_subscription_notifier.rb
+++ app/notifiers/thread_subscription_notifier.rb
@@ -9,12 +9,19 @@ class ThreadSubscriptionNotifier < Notifier
 
   def notify(email_recipients)
     unless email_recipients.empty?
-      mail = NotificationMailer.new_post_in_subscribed_thread_email(email_recipients, post)
-      BatchMailSender.new(mail).delay.deliver
+      Delayed::Job.enqueue ThreadSubscriptionJob.new(email_recipients, post)
     end
   end
 
   def possible_recipients
     @possible_recipients ||= post.thread.subscribers.select { |u| Ability.new(u).can? :read, post }.to_set
+  end
+
+private
+  ThreadSubscriptionJob = Struct.new(:email_recipients, :post) do
+    def perform
+      mail = NotificationMailer.new_post_in_subscribed_thread_email(email_recipients, post)
+      BatchMailSender.new(mail).deliver
+    end
   end
 end
