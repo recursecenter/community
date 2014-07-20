009830e8406f891a739f7bd06103e1d41018f526
Sonali Sridhar <sonali.sridhar@gmail.com>
2014-07-20 13:15:12 -0400

    Adjusted the col's to be sm-col rather than large-col. Replaced tables on index with ul's

--- client/src/community/components/index.cljs
+++ client/src/community/components/index.cljs
@@ -14,14 +14,13 @@
    [:li {:key id}
     [:h2 name]
     (if (not (empty? subforums))
-      [:table.table.table-striped
-       [:thead
-        [:tr [:th "Subforum"]]]
-       [:tbody
+      [:div.row
+       [:div.block-grid
         (for [{:keys [id slug] :as subforum} subforums]
-          [:tr {:key id :className (if (:unread subforum) "unread")}
-           [:td (link-to (routes :subforum {:id id :slug slug})
-                         (:name subforum))]])]]) ]))
+          [:ul
+          [:li {:key id :className (if (:unread subforum) "unread")}
+           [:a (link-to (routes :subforum {:id id :slug slug})
+                         (:name subforum))]]])]]) ]))
 
 (defcomponent index [{:as app :keys [current-user subforum-groups]}
                      owner]
