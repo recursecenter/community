148282995dde5a6517dd1cb79915525d70368892
Zachary Allaun <zach.allaun@gmail.com>
2014-07-20 00:48:28 -0400

    Add comments documenting a hack

--- client/src/community/components/shared.cljs
+++ client/src/community/components/shared.cljs
@@ -49,6 +49,8 @@
     (let [menu-showing? (and focused? (seq ac-selections))
           control-keys #{"ArrowUp" "ArrowDown" "Enter" "Tab"}]
       (letfn [(set-ac-selections [e]
+                ;; HACK: update value before possible causing a local
+                ;; re-render (see #71)
                 (on-change (.. e -target -value))
                 ;; Don't set ac selections again when e.g. someone's
                 ;; scrolling through the results they already see
@@ -70,7 +72,10 @@
                   (om/set-state! owner :ac-selections [])
                   (om/set-state! owner :new-cursor-pos (ac/cursor-position ac))))
               (handle-autocomplete-action [e]
+                ;; HACK: update value before possible causing a local
+                ;; re-render (see #71)
                 (on-change (.. e -target -value))
+
                 (when menu-showing?
                   (when-let [key (control-keys (.-key e))]
                     (.preventDefault e)
