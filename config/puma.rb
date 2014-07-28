f8a33d74f063c1abc5903dce5e54702cf7baa91c
David Albert <davidbalbert@gmail.com>
2014-07-28 11:58:18 -0400

    Account for websocket thread in connection pool

--- config/puma.rb
+++ config/puma.rb
@@ -12,7 +12,9 @@ on_worker_boot do
   ActiveSupport.on_load(:active_record) do
     config = ActiveRecord::Base.configurations[Rails.env] ||
                 Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = ENV['MAX_THREADS'] || 16
+
+    # We add 1 to our connection pool to account for our WebSocket thread.
+    config['pool'] = Integer(ENV['MAX_THREADS'] || 1) + 1
     ActiveRecord::Base.establish_connection(config)
   end
 
