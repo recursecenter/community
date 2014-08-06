73ae47f517af57b224cd9096bf101d090a2639e3
Zachary Allaun <zach.allaun@gmail.com>
2014-08-06 16:19:59 -0400

    Refactor controller route handlers

--- client/src/community/controller.cljs
+++ client/src/community/controller.cljs
@@ -30,66 +30,32 @@
 (defmulti handle-route-changed (fn [app-state {route :route}] route)
   :default :page-not-found)
 
-(defmethod handle-route-changed :index
-  [app-state route-data]
+(defn load-from-api [app-state route-data key api-call]
   (go
     (try
       (swap! app-state assoc :loading? true)
       (swap! app-state assoc
-        :subforum-groups (<? (api/subforum-groups))
+        key (<? (api-call))
         :route-data route-data
         :loading? false)
       (state/remove-errors! :ajax)
 
       (catch ExceptionInfo e
-        (state/add-error! (-> e ex-data :error-info)))
+        (let [e-data (ex-data e)]
+          (if (== 404 (:status e-data))
+            (dispatch :route-changed {:route :page-not-found})
+            (state/add-error! (:error-info e-data)))))
 
       (catch Exception e
         (.log js/console e)))))
 
-(defmethod handle-route-changed :settings
-  [app-state route-data]
-  (swap! app-state assoc :route-data route-data))
-
-(defmethod handle-route-changed :subforum
-  [app-state route-data]
-  (go
-    (try
-      (swap! app-state assoc :loading? true)
-      (swap! app-state assoc
-        :subforum (<? (api/subforum (:id route-data)))
-        :route-data route-data
-        :loading? false)
-      (state/remove-errors! :ajax)
-
-      (catch ExceptionInfo e
-        (let [e-data (ex-data e)]
-          (if (== 404 (:status e-data))
-            (dispatch :route-changed {:route :page-not-found})
-            (state/add-error! (:error-info e-data))))))))
-
-(defmethod handle-route-changed :thread
-  [app-state route-data]
-  (go
-    (try
-      (swap! app-state assoc :loading? true)
-      (swap! app-state assoc
-        :thread (<? (api/thread (:id route-data)))
-        :route-data route-data
-        :loading? false)
-      (state/remove-errors! :ajax)
-
-      (catch ExceptionInfo e
-        (let [e-data (ex-data e)]
-          (if (== 404 (:status e-data))
-            (dispatch :route-changed {:route :page-not-found})
-            (state/add-error! (:error-info e-data))))))))
-
-(defmethod handle-route-changed :page-not-found
-  [app-state route-data]
-  (swap! app-state assoc :route-data {:route :page-not-found}))
-
-
+(defn handle-route-changed [app-state route-data]
+  (condp keyword-identical? (:route route-data)
+    :index    (load-from-api app-state route-data :subforum-groups api/subforum-groups)
+    :subforum (load-from-api app-state route-data :subforum #(api/subforum (:id route-data)))
+    :thread   (load-from-api app-state route-data :thread #(api/thread (:id route-data)))
+    :settings (swap! app-state assoc :route-data route-data)
+    (swap! app-state assoc :route-data {:route :page-not-found})))
 
 ;;; Main loop
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
