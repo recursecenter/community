4c76ed2c6f77950389626f7e4b2eddc64529441c
Zachary Allaun <zach.allaun@gmail.com>
2014-08-15 10:26:03 -0400

    Fix user role tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,10 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test "users can have many roles" do
     zach = users(:zach)
+    zach.roles = [roles(:everyone), roles(:full_hacker_schooler), roles(:admin)]
+
     hacker_schooler_1 = users(:hacker_schooler_1)
+    hacker_schooler_1.roles = [roles(:everyone)]
 
     assert hacker_schooler_1.has_roles?(roles(:everyone))
     assert_not hacker_schooler_1.has_roles?(roles(:admin))
