4d4cac058b4ebcda9b3de588f8fe25b5e485251a
Zachary Allaun <zach.allaun@gmail.com>
2014-08-15 10:50:11 -0400

    Fix post test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
   test "Users can only edit posts they wrote" do
+    posts(:zach_post_1).thread.subforum.required_role_ids = []
+    posts(:dave_post_1).thread.subforum.required_role_ids = []
+
     ability = Ability.new(users(:zach))
+
     assert ability.can? :update, posts(:zach_post_1)
     assert ability.cannot? :update, posts(:dave_post_1)
   end
