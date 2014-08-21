c1bf419bfe87927c41b14e13acd49a18bbcaf7a4
Zachary Allaun <zach.allaun@gmail.com>
2014-08-21 11:38:26 -0400

    Mentions are destroyed when their post is destroyed

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -10,4 +10,15 @@ class PostTest < ActiveSupport::TestCase
     assert ability.can? :update, posts(:zach_post_1)
     assert ability.cannot? :update, posts(:dave_post_1)
   end
+
+  test "associated mentions are destroyed with their post" do
+    user = users(:full_hacker_schooler)
+    p = discussion_threads(:created_by_full_hacker_schooler).
+      posts.create(author: user, body: "post body")
+    mention = p.mentions.create(user: user, mentioned_by: user)
+
+    p.destroy
+
+    assert mention.destroyed?
+  end
 end
