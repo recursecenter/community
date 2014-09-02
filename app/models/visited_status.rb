27be303340bad82d4d50e18a64cc796ac456ad0f
Zachary Allaun <zach.allaun@gmail.com>
2014-09-02 17:13:12 -0400

    Use validates_uniqueness_of instead of custom validator

--- app/models/visited_status.rb
+++ app/models/visited_status.rb
@@ -2,11 +2,5 @@ class VisitedStatus < ActiveRecord::Base
   belongs_to :user
   belongs_to :thread, class_name: 'DiscussionThread'
 
-  validate :unique_for_thread_and_user, on: :create
-
-  def unique_for_thread_and_user
-    unless self.class.where(user_id: user_id, thread: thread).empty?
-      errors[:base] << "There can be only one VisitedStatus for a given user and thread."
-    end
-  end
+  validates_uniqueness_of :user_id, scope: :thread_id
 end
