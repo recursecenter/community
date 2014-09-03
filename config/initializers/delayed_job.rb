1f3c26ec9dfc2dc6ed89addab6261b77a31b3169
David Albert <davidbalbert@gmail.com>
2014-09-03 17:10:58 -0400

    Reload classes in delayed job in development
    Also refactor SilenceDelayedJobQueries.

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,2 +1,15 @@
+require 'delayed_job_class_reloader'
+require 'silence_delayed_job_queries'
+
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.destroy_failed_jobs = false
+
+unless Rails.application.config.cache_classes
+  Delayed::Worker.plugins << DelayedJobClassReloader
+end
+
+if Rails.env.development?
+  class << Delayed::Backend::ActiveRecord::Job
+    prepend SilenceDelayedJobQueries
+  end
+end
