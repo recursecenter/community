9d28e2fae77ad785a81c2353da62b33eee3e4a2a
Surendran Mahendran <surenspost@gmail.com>
2014-09-10 00:26:12 -0400

    Fixing code review suggestions

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,14 +41,14 @@ groups.each do |group|
 end
 
 users = [
-  {:first_name => "Carl", :last_name => "Sagan", :email => "carl@palebluedot.com"},
-  {:first_name => "Albert", :last_name => "Einstein", :email => "albert@relativity.net"},
-  {:first_name => "Marie", :last_name => "Curie", :email => "marie@halflife.org"},
-  {:first_name => "Aung", :last_name => "San Sui", :email => "aungsui@liberation.org"}
+  {first_name: "Carl", last_name: "Sagan", email: "carl@palebluedot.com"},
+  {first_name: "Albert", last_name: "Einstein", email: "albert@relativity.net"},
+  {first_name: "Marie", last_name: "Curie", email: "marie@halflife.org"},
+  {first_name: "Aung", last_name: "San Sui", email: "aungsui@liberation.org"}
 ]
 
 users.each do |user|
-  user = User.create :first_name => user[:first_name], :last_name => user[:last_name], :email => user[:email]
+  user = User.create user
   user.groups = [Group.everyone, Group.current_hacker_schoolers, Group.faculty]
   user.roles << Role.all
   user.save!
