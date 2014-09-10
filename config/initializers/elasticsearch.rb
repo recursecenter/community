574e5e56052f2fdeffdfbbd72dc9bfc59d23b3de
Surendran Mahendran <surenspost@gmail.com>
2014-09-10 15:38:18 -0400

    Initialize elasticsearch active record models

---
+++ config/initializers/elasticsearch.rb
@@ -0,0 +1,7 @@
+require 'elasticsearch/model'
+ENV['ELASTICSEARCH_HOST'] ||= 'localhost'
+ENV['ELASTICSEARCH_PORT'] ||= 9200
+
+enable_logging = Rails.env.development? ? true : false
+
+Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_HOST'], port: ENV['ELASTICSEARCH_PORT'], log: enable_logging
