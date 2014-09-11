455b59ed71b1b58351b667bc6f60dd2db0ddb00b
Zachary Allaun <zach.allaun@gmail.com>
2014-09-11 14:37:03 -0400

    Show names instead of emails in su index; get rid of helper

--- app/controllers/admin/su_controller.rb
+++ app/controllers/admin/su_controller.rb
@@ -1,5 +1,10 @@
 class Admin::SuController < AdminController
   protect_from_forgery with: :exception
+
+  def index
+    @su_users = User.all.order(:first_name)
+  end
+
   def create
     user = User.where(id: params['user_id']).first
     session[:user_id] = user.id if user
--- app/helpers/admin/su_helper.rb
+++
@@ -1,7 +0,0 @@
-module Admin::SuHelper
-  def available_users
-    users = User.all.select(:id, :email).map do |user|
-      [user.email, user.id]
-    end
-  end
-end
