69f154f1c6d1bc7bf0efce0a9b592b311caa9424
Zachary Allaun <zach.allaun@gmail.com>
2014-10-14 12:26:51 -0400

    Insert notification jobs in a job.
    This cuts down the time it takes to respond to Post#create and Thread#create by a lot by inserting notification jobs into our delayed jobs table out of band.

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -9,15 +9,11 @@ class Api::PostsController < Api::ApiController
     @post.mark_as_visited(current_user)
     PubSub.publish :created, :post, @post
 
-    NotificationCoordinator.new(
-      MentionNotifier.new(@post, mentioned_users),
-      ThreadSubscriptionNotifier.new(@post),
-      BroadcastNotifier.new(@post)
-    ).notify
+    Delayed::Job.enqueue NewPostNotificationJob.new(@post, mentioned_user_ids)
   end
 
   def update
-    MentionNotifier.new(@post, mentioned_users).notify
+    MentionNotifier.new(@post, User.where(id: mentioned_user_ids)).notify
 
     @post.update!(update_params)
     PubSub.publish :updated, :post, @post
