72f87366bfbe97f1aa9e9f881171c10c0ab89a79
Surendran Mahendran <surenspost@gmail.com>
2014-10-15 01:45:11 -0400

    No need to parse filters in the query
    Filters will be passed in from the client along with the query

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -19,19 +19,19 @@ module Searchable
     end
 
     # Search method to query the index for this particular model
-    def search(query)
-      __elasticsearch__.search(query: self.query_dsl(query), highlight: self.highlight_fields)
+    def search(search_string, filters)
+      __elasticsearch__.search(query: self.query_dsl(search_string, filters), highlight: self.highlight_fields)
     end
 
     # Suggest methods to return suggestions for this particular model
-    def suggest(query)
-       suggest = { suggestions: { text: query, completion: { field: "suggest" } } }
+    def suggest(search_string)
+       suggest = { suggestions: { text: search_string, completion: { field: "suggest" } } }
       __elasticsearch__.client.suggest(index: self.table_name, body: suggest)["suggestions"].first["options"]
     end
 
     # Override this method to customize the DSL for querying the including model
-    def query_dsl(query)
-      return strip_filters(query)
+    def query_dsl(search_string, filters)
+      return search_string
     end
 
     # Override this method to change the fields to highlight when this model is queried
@@ -42,28 +42,6 @@ module Searchable
     # Override this method in the corresponding model to enable allowed filter keys
     def allowed_filter_fields
       return Array.new
-    end
-
-    # For every allowed filter key,
-    # return key-value pairs for patterns matching "key:(value)" pattern.
-    # value can have spaces.
-    def filters(query)
-      filters = Hash.new
-      allowed_filter_fields.each do |filter_key|
-        matches = /(?<key>#{filter_key}):\((?<value>[^\)]*)\)/.match(query)
-        filters[matches[:key]] = matches[:value] unless matches.blank?
-      end
-      return filters
-    end
-
-    # Removes all filters from query and just return plain query
-    def strip_filters(query)
-      matches = /(([A-Za-z_]+):\(([^\)]+)\) )+(?<text>.*)/.match(query)
-      if filters(query).blank? && matches.blank?
-        return query
-      else
-        return matches[:text]
-      end
     end
   end
 
