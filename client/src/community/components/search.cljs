00dd128039ebfb7ad04fc2c4bbe6e481ab3f0c29
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-10-27 17:20:45 -0400

    WIP - Modifying components structure

--- client/src/community/components/search.cljs
+++ client/src/community/components/search.cljs
@@ -45,10 +45,6 @@
 (defn display [show]
   (if show {} {:display "none"}))
 
-(defn clear! [query-data]
-  (reset! query-data {:text ""
-                      :filters {:author nil :subforum nil :thread nil}}))
-
 (defn jump-to-page [{:keys [search-filter text id slug]}]
   (routes/redirect-to 
     (condp = search-filter
@@ -71,40 +67,37 @@
 
 (defn complete-and-respond! [query-data selected]
   (cond 
+    (nil? selected)
+      (search! query-data)
     (= :author (:search-filter selected))
       (search! (complete-suggestion query-data selected))    
     (contains? #{:thread :subforum} (:search-filter selected))
       (jump-to-page selected)))
 
-(defcomponent suggestions-view [app owner]
-  (display-name [_] "Search suggestions")
-  (render-state [_ {:keys [query-data suggestions show-suggestions?]}]
-    (html
-     [:ol
-      {:id "suggestions" :ref "suggestions"
-       :style (display (and show-suggestions? (not (empty? (:text query-data)))
-                                              (not (empty? suggestions))))}
-        (for [{:keys [selected? value] :as suggestion} suggestions]
-          [:li {:class (when selected? "selected")
-                :onClick (fn [e]
-                           (.preventDefault e)
-                           (complete-and-respond! query-data value))
-                :data-search-filter (when (= 0 (:count value)) (name (:search-filter value)))} 
-            (:text value)])])))
+(defn suggestions-dropdown [{:keys [query-data suggestions show-suggestions?]}]
+ (html
+   [:ol
+    {:id "suggestions" :ref "suggestions"
+     :style (display (and show-suggestions? (not (empty? (:text query-data)))
+                                            (not (empty? suggestions))))}
+      (for [{:keys [selected? value] :as suggestion} suggestions]
+        [:li {:class (when selected? "selected")
+              :onClick (fn [e]
+                         (.preventDefault e)
+                         (complete-and-respond! query-data value))
+              :data-search-filter (when (= 0 (:count value)) (name (:search-filter value)))} 
+          (:text value)])]))
 
 (defcomponent input-view 
-  [{:keys [query-data show-suggestions! select! complete! query-text-change! complete-and-respond! clear!]}
-                          owner]
+  [{:keys [query-data show-suggestions! select! complete! query-text-change! complete-and-respond! ]}
+   owner]
   (display-name [_] "Search Input")
 
   (render [_]
     (html
        [:form
         {:id "search-form"
-         :name "search-form"
-         :onSubmit (fn [e]
-                     (.preventDefault e)
-                     (complete-and-respond!))}
+         :name "search-form"}
         [:i {:id "search-icon" :class "fa fa-search"}]
         [:input.form-control {:ref "search-query"
                               :type "search"
@@ -117,14 +110,16 @@
                                           (query-text-change! (.. e -target -value)))
                               :onKeyDown (fn [e]
                                            (let [keycode (.-keyCode e)]
-                                             (when (contains? #{UP_ARROW DOWN_ARROW ENTER} keycode)
+                                             (prn query-data)
+                                             (when (contains? #{UP_ARROW DOWN_ARROW ENTER ESC} keycode)
                                                (.preventDefault e)
                                                (condp = keycode
                                                  DOWN_ARROW (select! :next)
                                                  UP_ARROW (select! :prev)
-                                                 ENTER (do
+                                                 ENTER (do 
                                                          (complete-and-respond!)
-                                                         (clear!))))))}]])))
+                                                         (.blur (om/get-node owner "search-query")))
+                                                 ESC (if )))))}]])))
 (def initial-query-state {:text ""
                           :filters {:author nil :subforum nil :thread nil}})
 
@@ -159,11 +154,8 @@
                                            (om/update-state! owner :query-data #(assoc % :text text))
                                            (controller/dispatch :update-search-suggestions text))
                      :complete-and-respond! (fn []
-                                                (if-let [selected (sl/selected suggestions)]
-                                                  (complete-and-respond! query-data selected)
-                                                  (search! query-data)))
-                     :clear! (fn []
-                               (om/set-state! owner :query-data initial-query-state))})
+                                                (let [selected (sl/selected suggestions)]
+                                                  (complete-and-respond! query-data selected)))})
       (->suggestions-view app {:state state})])))
 
 (defcomponent result [{:keys [post author thread subforum highlight] :as result}]
@@ -191,17 +183,26 @@
        [:div.body 
         (partials/html-from-markdown highlight)]]])))
 
+(defn query->display [{:keys [author]} query]
+  (str
+    (if-not (empty? author) (str "Author: " author) "")
+    "|"
+    (if-not (empty? query) (str "Query: " query) "")))
+
 (defcomponent search-results [{:keys [search] :as app} owner]
   (display-name [_] "Search Results")
 
   (render [_]
     (let [results (:results search)
-          metadata (:metadata search)]
+          {:keys [result-count took query filters]} (:metadata search)]
       (if (empty? results)
         (html
          [:div
           "Sorry, there were no matching results for this search."])
         (html
          [:div {:id "search-results-view"}
-          [:div.query "Search Results for : " ]
+          [:div.query (str "Search Results for - " 
+                           (query->display filters query)
+                           " - Fetched n" result-count " results "
+                           "in " took " ms.")]
           [:div.results (map (partial ->result) results)]])))))
