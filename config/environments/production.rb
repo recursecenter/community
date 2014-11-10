a22ac8766d0d490afe4eb5a279f775f81f0504e9
Zachary Allaun <zach.allaun@gmail.com>
2014-11-10 18:52:00 -0500

    Log request UUIDs in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
+  config.log_tags = [ :uuid ]
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
