2f67aca69a764778b71f9b3ecc952bb73f135062
David Albert <davidbalbert@gmail.com>
2014-11-12 14:43:43 -0500

    Simplify Elasticsearch config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,6 +2,4 @@ require 'elasticsearch/model'
 ENV['ELASTICSEARCH_HOST'] ||= 'localhost'
 ENV['ELASTICSEARCH_PORT'] ||= '9200'
 
-enable_logging = Rails.env.development? ? true : false
-
-Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_HOST'], port: ENV['ELASTICSEARCH_PORT'], log: enable_logging
+Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['ELASTICSEARCH_HOST'], port: ENV['ELASTICSEARCH_PORT'], log: Rails.env.development?
