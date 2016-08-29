4f31917b58c7f2ee1e55d14e3c4fc84e58a1d99d
Zachary Allaun <zach.allaun@gmail.com>
2016-08-29 14:32:37 -0400

    Add `lax_spacing: true` to Redcarpet config to fix lists w/o newline

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
   def markdown(md)
-    markdown_renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new, autolink: true, fenced_code_blocks: true, no_intra_emphasis: true)
+    markdown_renderer = Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML.new,
+      autolink: true,
+      fenced_code_blocks: true,
+      no_intra_emphasis: true,
+      lax_spacing: true
+    )
+
     sanitize(markdown_renderer.render(md))
   end
 end
