9b9a733510b4e7fd0b52a914bb8dce684cd8f174
Zachary Allaun <zach.allaun@gmail.com>
2017-02-02 13:31:34 -0500

    Switch back to BONSAI_URL

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,3 @@
-ENV['ELASTICSEARCH_URL'] ||= ENV['BONSAI_IVORY_URL'] || 'http://localhost:9200'
+ENV['ELASTICSEARCH_URL'] ||= ENV['BONSAI_URL'] || 'http://localhost:9200'
 
 Elasticsearch::Model.client = Elasticsearch::Client.new log: Rails.env.development?
