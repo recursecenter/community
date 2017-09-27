843f6b65a5b8d2dd8a70667f05f1297b4e99d388
David Albert <davidbalbert@gmail.com>
2017-09-27 11:51:33 -0400

    Don't use strings when adding middleware

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require_relative '../lib/web_socket_handler'
+
 module Community
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -14,7 +16,7 @@ module Community
 
     config.active_record.schema_format = :sql
 
-    config.middleware.use "WebSocketHandler"
+    config.middleware.use WebSocketHandler
   end
 end
 
