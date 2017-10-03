97a2887374ea38e50d970c334415ed5bb82a1bf1
David Albert <davidbalbert@gmail.com>
2017-10-03 11:36:48 -0400

    modernize routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,35 +2,35 @@ Rails.application.routes.draw do
   root 'pages#index'
 
   resources :sessions
-  get 'login' => 'sessions#new'
-  get 'logout' => 'sessions#destroy'
-  get 'login/complete' => 'sessions#complete'
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+  get 'login/complete', to: 'sessions#complete'
 
   # client side routes
-  get '/settings' => 'pages#index'
-  get '/f/*path' => 'pages#index'
-  get '/t/:slug/:id' => 'pages#index', as: 'thread'
-  get '/t/:slug/:thread_id/:post_number' => 'pages#index', as: 'post'
-  get '/s' => 'pages#index'
-  get '/s/:query' => 'pages#index'
+  get '/settings', to: 'pages#index'
+  get '/f/*path', to: 'pages#index'
+  get '/t/:slug/:id', to: 'pages#index', as: 'thread'
+  get '/t/:slug/:thread_id/:post_number', to: 'pages#index', as: 'post'
+  get '/s', to: 'pages#index'
+  get '/s/:query', to: 'pages#index'
 
-  get '/threads/:id/unsubscribe' => 'threads#unsubscribe'
-  get '/threads/:id/subscribe'   => 'threads#subscribe'
-  get '/threads/unsubscribe/:token' => 'threads#unsubscribe_with_reply_info', as: :unsubscribe_thread
-  get '/threads/subscribe/:token'   => 'threads#subscribe_with_reply_info',   as: :subscribe_thread
+  get '/threads/:id/unsubscribe', to: 'threads#unsubscribe'
+  get '/threads/:id/subscribe', to: 'threads#subscribe'
+  get '/threads/unsubscribe/:token', to: 'threads#unsubscribe_with_reply_info', as: :unsubscribe_thread
+  get '/threads/subscribe/:token', to: 'threads#subscribe_with_reply_info', as: :subscribe_thread
 
   # sudo for development only
   namespace :admin do
     if Rails.env.development?
-      get 'su' => 'su#index'
-      post 'su' => 'su#create'
+      get 'su', to: 'su#index'
+      post 'su', to: 'su#create'
     end
   end
 
   namespace :api, format: false, defaults: {format: 'json'} do
-    post 'welcome_message/read' => 'welcome_messages#read'
-    get 'search' => 'search#search'
-    get 'suggestions' => 'search#suggestions'
+    post 'welcome_message/read', to: 'welcome_messages#read'
+    get 'search', to: 'search#search'
+    get 'suggestions', to: 'search#suggestions'
 
     resources :users do
       get :me, on: :collection
