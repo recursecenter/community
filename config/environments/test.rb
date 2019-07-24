12f72128f5aaef09f2c6e410ab7e9b629d22c958
David Albert <davidbalbert@gmail.com>
2019-07-24 13:07:05 -0400

    Don't print queries in test output

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,5 +40,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  config.log_level = :warn
+
   config.action_mailer.default_url_options = { host: "localhost", port: "5001" }
 end
