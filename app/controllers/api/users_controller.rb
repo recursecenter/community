4641f670de123208f44e0610918435cb712a511a
James Porter <james@jamesporter.me>
2020-11-10 10:32:37 -0500

    use secure compare

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -19,7 +19,7 @@ class Api::UsersController < Api::ApiController
   private
 
   def authorize_using_hacker_school_secret_token
-    if HackerSchool.secret_token != params[:secret_token]
+    if !ActiveSupport::SecurityUtils.secure_compare(HackerSchool.secret_token, params[:secret_token])
       render json: {message: 'not_found'}, status: 404
     end
   end
