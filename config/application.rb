a21f2284622863bf0d40e37c0aab357084b219e3
David Albert <davidbalbert@gmail.com>
2023-01-19 15:52:04 -0500

    Rails 6 + app:update

--- config/application.rb
+++ config/application.rb
@@ -7,13 +7,19 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 require_relative '../lib/web_socket_handler'
+require_relative '../lib/event_machine_smtp_delivery'
 
 module Community
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.autoloader = :zeitwerk
 
     # Settings in config/environments/* take precedence over those specified here.
+
+    # Not default, but recommended by the Configuring Rails
+    # Applications guide.
+    config.add_autoload_paths_to_load_path = false
 
     config.active_record.schema_format = :sql
 
@@ -25,19 +31,15 @@ module Community
     # require 'syslog/logger'
     # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-    if ENV["RAILS_LOG_TO_STDOUT"].present?
-      logger           = ActiveSupport::Logger.new(STDOUT)
-      logger.formatter = config.log_formatter
-      config.logger    = ActiveSupport::TaggedLogging.new(logger)
-    end
-
     config.middleware.use WebSocketHandler
 
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.after_initialize do
+      ActionMailer::Base.add_delivery_method :eventmachine_smtp, EventMachineSmtpDelivery
+    end
   end
 end
 
-require 'event_machine_smtp_delivery'
-ActionMailer::Base.add_delivery_method :eventmachine_smtp, EventMachineSmtpDelivery
