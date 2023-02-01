d6abe2672919cb9b59f6562e7dfca708b870a275
David Albert <davidbalbert@gmail.com>
2023-02-01 12:47:11 -0500

    Pass in relations to mailer methods in preview

--- test/mailers/previews/notification_mailer_preview.rb
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -5,18 +5,18 @@ class NotificationMailerPreview < ActionMailer::Preview
   end
 
   def broadcast_email
-    NotificationMailer.broadcast_email(User.first, Post.first)
+    NotificationMailer.broadcast_email(User.limit(1), Post.first)
   end
 
   def new_post_in_subscribed_thread_email
-    NotificationMailer.new_post_in_subscribed_thread_email(User.first, Post.first)
+    NotificationMailer.new_post_in_subscribed_thread_email(User.limit(1), Post.first)
   end
 
   def new_thread_in_subscribed_subforum_email
-    NotificationMailer.new_thread_in_subscribed_subforum_email(User.first, DiscussionThread.first)
+    NotificationMailer.new_thread_in_subscribed_subforum_email(User.limit(1), DiscussionThread.first)
   end
 
   def new_subscribed_thread_in_subscribed_subforum_email
-    NotificationMailer.new_subscribed_thread_in_subscribed_subforum_email(User.first, DiscussionThread.first)
+    NotificationMailer.new_subscribed_thread_in_subscribed_subforum_email(User.limit(1), DiscussionThread.first)
   end
 end
