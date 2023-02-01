3cc62574a8ed6d6701459f30243d34fe62a3596d
David Albert <davidbalbert@gmail.com>
2023-02-01 13:35:35 -0500

    Remove SMTP_TLS flag and always assume Implicit TLS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
     port: ENV["SMTP_PORT"],
     user_name: ENV["SMTP_USERNAME"],
     password: ENV["SMTP_PASSWORD"],
-    tls: ENV["SMTP_TLS"].present?,
+    tls: true,
     authentication: :plain,
     domain: 'mail.community.recurse.com',
   }
