eadc2f76af89af9b2587bed2fb0014546a056641
David Albert <davidbalbert@gmail.com>
2023-02-13 14:57:36 -0500

    Print more debugging info on boot

--- config/boot.rb
+++ config/boot.rb
@@ -6,5 +6,12 @@ Timeout.timeout(5) do
   sleep(1)
 end
 
+puts "!!!!!!!!!!!"
+puts Thread.list.map { |t| "#{t} - #{t.group == ThreadGroup::Default ? "DEFAULT" : "no"}" }.join("\n")
+
+
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+puts "+++++++++++"
+puts Thread.list.map { |t| "#{t} - #{t.group == ThreadGroup::Default ? "DEFAULT" : "no"}" }.join("\n")
