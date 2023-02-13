c412b933ef3b846d588b9693feded932a110584b
David Albert <davidbalbert@gmail.com>
2023-02-13 10:34:07 -0500

    I think this is the earliest we could possibly run this code?

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: bin/rails server
+web: bin/puma -C config/puma.rb
 worker: bin/rails jobs:work
 release: bin/rails db:migrate
--- config/boot.rb
+++ config/boot.rb
@@ -1,9 +1,4 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
-
-# https://github.com/airbrake/airbrake-ruby/issues/713
-require 'timeout'
-Timeout.ensure_timeout_thread_created
-
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
