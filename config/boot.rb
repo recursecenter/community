e9b39d912951a58a7d976dd37be802b948de468f
David Albert <davidbalbert@gmail.com>
2023-02-13 14:52:22 -0500

    Attempt a different workaround

--- config/boot.rb
+++ config/boot.rb
@@ -2,7 +2,9 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 # https://github.com/airbrake/airbrake-ruby/issues/713
 require 'timeout'
-Timeout.ensure_timeout_thread_created
+Timeout.timeout(5) do
+  sleep(1)
+end
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
