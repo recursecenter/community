4e962b6a68f652c7a8be7aea1bb42b39ecc1cfd9
David Albert <davidbalbert@gmail.com>
2023-03-13 14:39:47 -0400

    Rails 7 default config

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require_relative "../lib/thread_error_logger"
 module Community
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
